f9ac18570d70e551baa3758bf64dfea2b8845e47
Satyam <satyamchauhan9656@gmail.com>
2025-01-01 20:14:15 +0530

    I did some changes in hari.c to make it useful and add in 49tut.c

--- hari.c
+++ hari.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-
 int fac(int n){
     if(n ==0 || n ==1 ){
         return 1;
     }else{
         return (n * fac(n-1) ) ;
     }
-}
-
-int main()
-{    int num ;
-    printf("enter a number you want to find the factoriL OF : \n ");
-    scanf("%d",&num);
-    printf("%d is the factorial of %d",fac(num),num);
-     return 0 ;
 }
