d38d02de3d8b8aff050c70a15e0010ba2843ff00
Satyam <satyamchauhan9656@gmail.com>
2025-01-01 20:55:23 +0530

    These are some other files

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
-void main ()
+#include <stdio.h>
+#include "facto.c" // I include this file from my system to get some function in my this code .
+
+// int sum; // Global variable or we can say IT is a External variable (External storage class)
+
+int add(int a, int b)
 {
-int a,b,c;
-printf("enter the values of a and b\n");
-scanf("%d,%d",&a,&b);aaaaaaaaaaaaaaaaaas
-c=a;
-a=b;
-b=c;
-printf("values after swap\n");
-printf("a=%d, b=%d",a,b);
+    // auto int sum; // or you can right "auto int sum" ,it id the same thing .
+    extern int sum ;
+    // sum = a + b;
+    return sum;
+}
+int sum = 343;
+int main()
+{
+    int sum = add(9, 7);
+    printf("%d\n", sum);
+    printf("%d\n", fac(5));
+    return 0;
 }
